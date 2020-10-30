2b9c409e0e6c794a3c01821642f6c5723ca3c2c7
miguel <miguel.silva@ifood.com.br>
2020-10-30 18:22:50 -0300

    removendo arquivos eclipse

--- src/main/java/br/com/customerAccount/dao/ClienteDAO.java
+++ src/main/java/br/com/customerAccount/dao/ClienteDAO.java
@@ -1,63 +1,60 @@
 package br.com.customerAccount.dao;
 
-import java.util.List;
-
-import javax.persistence.Query;
-
 import br.com.customerAccount.model.Cliente;
 
+import javax.persistence.Query;
+import java.util.List;
+
 /**
- * 
  * @author miguel
- * 
- *         Classe de persistencia da classe Cliente, extende as funcionalidade
- *         da GenericDAO
- *
+ * <p>
+ * Classe de persistencia da classe Cliente, extende as funcionalidade
+ * da GenericDAO
  */
 public class ClienteDAO extends GenericDAO<Cliente> {
 
-	public ClienteDAO() {
-		super(Cliente.class);
-	}
+    public ClienteDAO() {
+        super(Cliente.class);
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<Cliente> listaByIntervalId(int min, int max) {
-		reset();
-		StringBuilder hql = new StringBuilder();
+    @SuppressWarnings("unchecked")
+    public List<Cliente> listaByIntervalId(int min, int max) {
+        reset();
+        StringBuilder hql = new StringBuilder();
 
-		hql.append(" SELECT c ");
-		hql.append(" FROM ");
-		hql.append(" 	Cliente c ");
-		hql.append(" WHERE ");
-		hql.append(" 	 c.id >= :min ");
-		hql.append(" AND c.id <= :max ");
-		hql.append(" ORDER BY c.saldo DESC");
+        hql.append(" SELECT c ");
+        hql.append(" FROM ");
+        hql.append(" 	Cliente c ");
+        hql.append(" WHERE ");
+        hql.append(" 	 c.id >= :min ");
+        hql.append(" AND c.id <= :max ");
+        hql.append(" ORDER BY c.saldo DESC");
 
-		Query q = manager.createQuery(hql.toString());
-		q.setParameter("min", min);
-		q.setParameter("max", max);
+        Query q = manager.createQuery(hql.toString());
+        q.setParameter("min", min);
+        q.setParameter("max", max);
 
-		return q.getResultList();
-	}
+        return q.getResultList();
+    }
 
-	public double mediaSaldoCliente(int idMin, int idMax, double minSaldo) {
-		reset();
-		StringBuilder hql = new StringBuilder();
+    public double mediaSaldoCliente(int idMin, int idMax, double minSaldo) {
+        reset();
+        StringBuilder hql = new StringBuilder();
 
-		hql.append(" SELECT COALESCE(AVG(c.saldo),0) ");
-		hql.append(" FROM ");
-		hql.append(" 	Cliente c ");
-		hql.append(" WHERE ");
-		hql.append(" 	 c.id >= :min ");
-		hql.append(" AND c.id <= :max ");
-		hql.append(" AND c.saldo > :minSaldo ");
+        hql.append(" SELECT COALESCE(AVG(c.saldo),0) ");
+        hql.append(" FROM ");
+        hql.append(" 	Cliente c ");
+        hql.append(" WHERE ");
+        hql.append(" 	 c.id >= :min ");
+        hql.append(" AND c.id <= :max ");
+        hql.append(" AND c.saldo > :minSaldo ");
 
-		Query q = manager.createQuery(hql.toString());
-		
-		q.setParameter("min", idMin)
-		.setParameter("max", idMax)
-		.setParameter("minSaldo", minSaldo);
+        Query q = manager.createQuery(hql.toString());
 
-		return (double) q.getSingleResult();
-	}
+        q.setParameter("min", idMin)
+                .setParameter("max", idMax)
+                .setParameter("minSaldo", minSaldo);
+
+        return (double) q.getSingleResult();
+    }
 }
--- src/main/java/br/com/customerAccount/model/Cliente.java
+++ src/main/java/br/com/customerAccount/model/Cliente.java
@@ -9,83 +9,81 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 /**
- * 
  * @author miguel
- * 
- *         POJO contendo atributos da tabela cliente
- *
+ * <p>
+ * POJO contendo atributos da tabela cliente
  */
 @Entity
 @Table(name = "tb_customer_account", schema = "public")
 public class Cliente {
 
-	@Id
-	@Column(name = "id_customer")
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "clientes_seq_gen")
-	@SequenceGenerator(name = "clientes_seq_gen", sequenceName = "cliente_id_seq")
-	private int id;
+    @Id
+    @Column(name = "id_customer")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "clientes_seq_gen")
+    @SequenceGenerator(name = "clientes_seq_gen", sequenceName = "cliente_id_seq")
+    private int id;
 
-	@Column(name = "cpf_cnpj", unique = true)
-	private int cpf_cnpj;
+    @Column(name = "cpf_cnpj", unique = true)
+    private int cpf_cnpj;
 
-	@Column(name = "nm_customer")
-	private String nome;
+    @Column(name = "nm_customer")
+    private String nome;
 
-	@Column(name = "is_active")
-	private boolean ativo;
+    @Column(name = "is_active")
+    private boolean ativo;
 
-	@Column(name = "vl_total")
-	private double saldo;
+    @Column(name = "vl_total")
+    private double saldo;
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public int getCpf_cnpj() {
-		return cpf_cnpj;
-	}
+    public int getCpf_cnpj() {
+        return cpf_cnpj;
+    }
 
-	public void setCpf_cnpj(int cpf_cnpj) {
-		this.cpf_cnpj = cpf_cnpj;
-	}
+    public void setCpf_cnpj(int cpf_cnpj) {
+        this.cpf_cnpj = cpf_cnpj;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public boolean isAtivo() {
-		return ativo;
-	}
+    public boolean isAtivo() {
+        return ativo;
+    }
 
-	public void setAtivo(boolean ativo) {
-		this.ativo = ativo;
-	}
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
 
-	public double getSaldo() {
-		return saldo;
-	}
+    public double getSaldo() {
+        return saldo;
+    }
 
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder cliente = new StringBuilder();
+    @Override
+    public String toString() {
+        StringBuilder cliente = new StringBuilder();
 
-		cliente.append("\nCliente.: " + id);
-		cliente.append("\nNome....: " + nome);
-		cliente.append("\nCPF.....: " + cpf_cnpj);
-		cliente.append("\nSaldo...: " + saldo);
+        cliente.append("\nCliente.: " + id);
+        cliente.append("\nNome....: " + nome);
+        cliente.append("\nCPF.....: " + cpf_cnpj);
+        cliente.append("\nSaldo...: " + saldo);
 
-		return cliente.toString();
-	}
+        return cliente.toString();
+    }
 }
